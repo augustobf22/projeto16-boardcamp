88b7cccc5bd13248c07fd1ab79b1e62ef0416e62
augustof <augustobfanck@gmail.com>
2023-07-30 17:12:28 -0300

    bug: get clients fixed

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.connection.js"
 export async function getCustomers(req, res) {
     try {
         const customers = await db.query(`SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY-MM-DD') AS birthday FROM customers;`);
-        res.status(200).send(customers.rows[0]);
+        res.status(200).send(customers.rows);
     } catch (err) {
         res.status(500).send(err.message);
     }
